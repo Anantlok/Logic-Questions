74a7590c060a580414f43a6f6cbc74d0fa977e2b
Anant <167135027+Anantlok@users.noreply.github.com>
2025-08-18 23:25:10 +0530

    Time: 327 ms (5.03%) | Memory: 13.8 MB (40.74%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        s=s.lower()
+        p=""
+        for i in s:
+            if i.isalnum():
+                p=p+i
+        if p=="".join(reversed(p)):
+            return True
+        else:
+            return False

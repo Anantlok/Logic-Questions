829d8960a7d402e403c991e188f460eadcc63b58
Anant <167135027+Anantlok@users.noreply.github.com>
2025-12-30 23:59:06 +0530

    Update maximum-69-number.py

--- 1448-maximum-69-number/maximum-69-number.py
+++ 1448-maximum-69-number/maximum-69-number.py
@@ -4,7 +4,7 @@ class Solution(object):
         maxi = num
         for i in range(len(nums)):
             temp=nums[:]
-            if temp[i] == "6":
+            if temp[i] == "":
                 temp[i] = "9"
             elif temp[i] == "9":
                 temp[i] = "6"

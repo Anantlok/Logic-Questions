a33b54f4f215642b5be723cb844f8c196706921b
Anant <167135027+Anantlok@users.noreply.github.com>
2026-01-15 18:55:37 +0530

    Time: 3 ms (62.03%), Space: 19.3 MB (13.51%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -5,12 +5,11 @@ class Solution:
         ans=0
         while left<=right:
             mid=(left+right)//2
-            if (mid*mid)==x:
+            if mid*mid==x:
                 return mid
-            elif (mid*mid)<x:
+            elif mid*mid<x:
                 ans=mid
-                left=mid+1
+                left=mid+1 
             else:
                 right=mid-1
         return ans
-        

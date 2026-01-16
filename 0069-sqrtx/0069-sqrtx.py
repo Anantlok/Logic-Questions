965af1abb58816bfd386adea18d2ecc153dac342
Anant <167135027+Anantlok@users.noreply.github.com>
2026-01-16 13:07:26 +0530

    Time: 1 ms (70.98%), Space: 19.3 MB (15.08%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -9,7 +9,7 @@ class Solution:
                 return mid
             elif mid*mid<x:
                 ans=mid
-                left=mid+1 
+                left=mid+1                
             else:
                 right=mid-1
         return ans

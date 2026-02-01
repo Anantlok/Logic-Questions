6af64df8e0c266a802b67108ad7fa3bf3fb0ce14
Anant <167135027+Anantlok@users.noreply.github.com>
2026-02-01 22:07:33 +0530

    Time: 0 ms (100%), Space: 19.4 MB (32.83%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -1,13 +1,12 @@
 class Solution:
     def minimumCost(self, nums: List[int]) -> int:
-        k=nums[0]
-        def bubblesort(arr):
-            for i in range(0,len(arr)):
-                for j in range(len(arr)-i-1):
-                    if arr[j+1]<arr[j]:
-                        arr[j],arr[j+1]=arr[j+1],arr[j]
-            return arr
-        rest=bubblesort(nums[1:])
-        print(k,rest[0],rest[1])
-        return k+rest[0]+rest[1]
+        i=float('inf')
+        j=float('inf')
+        for x in nums[1:]:
+            if x<i:
+                j=i
+                i=x
+            elif x<j:
+                j=x
+        return nums[0]+i+j
         

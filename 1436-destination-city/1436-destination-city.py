f614dd7c4baf663796a8f6f48cd9c68c07961c39
Anant <167135027+Anantlok@users.noreply.github.com>
2026-03-26 23:31:56 +0530

    Time: 0 ms (100%), Space: 19.4 MB (21.8%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.py
@@ -0,0 +1,11 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        start_cities = set()
+
+        for a, b in paths:
+            start_cities.add(a)
+
+        for a, b in paths:
+            if b not in start_cities:
+                return b
+        
